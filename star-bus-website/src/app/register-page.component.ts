627338b3b1b2505e048b2dc95e74a2e302108338
vanaja <1jareddy55@gmail.com>
2019-02-13 15:08:05 +0530

    Validation

--- star-bus-website/src/app/register-page.component.ts
+++ star-bus-website/src/app/register-page.component.ts
@@ -10,17 +10,37 @@ import { UserService } from './register-page-service';
 
  export class AddUserComponent{
      users :User = new User();
+     confirmpass:String;
+     array={password:""};
 
      constructor( public ur:UserService){
 
    }
+   
+   keyPress(event: any) {
+    const pattern = /[0-9\+\-\ ]/;
+    let inputChar = String.fromCharCode(event.charCode);
+    if (event.keyCode != 8 && !pattern.test(inputChar)) {
+      event.preventDefault();
+    }
+  }
+
 
      store(){
+       let check=true;
+      if (this.confirmpass != this.users.password) {
+        this.array['password'] 
+        check=false;
+        alert("password does not match!!");
+    }
+
+    if(check) {
       let url='http://localhost:8181/bus/add'
       this.ur.sendToServer(url,this.users).subscribe(data=>{
       
       });
           }
+      }
 }
 
  
